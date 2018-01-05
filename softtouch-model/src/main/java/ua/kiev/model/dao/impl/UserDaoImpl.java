b9986292fb5eae8ab28cbb1ad9b9b07e77a3b82b
firsov <firsov@firs.kievreclama.local>
2018-01-05 16:58:51 +0200

    i'm creating method byemail

--- softtouch-model/src/main/java/ua/kiev/model/dao/impl/UserDaoImpl.java
+++ softtouch-model/src/main/java/ua/kiev/model/dao/impl/UserDaoImpl.java
@@ -2,15 +2,30 @@ package ua.kiev.model.dao.impl;
 
 import java.util.List;
 
+import javax.persistence.NoResultException;
+import javax.persistence.TypedQuery;
+
+import org.hibernate.Session;
+import org.springframework.transaction.annotation.Transactional;
+
 import ua.kiev.model.dao.UserDao;
 import ua.kiev.model.entities.User;
 
 public class UserDaoImpl extends GenericMainDaoImpl<User> implements UserDao{
 
+	@Transactional
 	@Override
 	public User byEmail(String emeil) {
-		// TODO Auto-generated method stub
-		return null;
+		Session session = sessionFactory.getCurrentSession();
+		String hql = "SELECT u FROM User u WHERE u.username = :email";
+		TypedQuery<User> query = session.createQuery(hql, User.class);
+		query.setParameter("email", emeil);
+		try {
+			return query.getSingleResult();
+		}
+		catch(NoResultException e) {
+			return null;
+		}
 	}
 
 	@Override
