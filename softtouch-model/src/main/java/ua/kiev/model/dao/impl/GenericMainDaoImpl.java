9e209b4dbc7aa85913e09e9075e5f6cd296cd778
firsov <firsov@firs.kievreclama.local>
2018-01-15 11:56:13 +0200

    S'm removing transaction

--- softtouch-model/src/main/java/ua/kiev/model/dao/impl/GenericMainDaoImpl.java
+++ softtouch-model/src/main/java/ua/kiev/model/dao/impl/GenericMainDaoImpl.java
@@ -3,7 +3,6 @@ package ua.kiev.model.dao.impl;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.transaction.annotation.Transactional;
 
 import ua.kiev.model.dao.GenericMainDao;
 
